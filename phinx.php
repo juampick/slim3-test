e57cb6c9166aeccc2f1e81dcced7ee7382cda822
Juan Pablo Cook <juampick@gmail.com>
2017-08-02 00:01:21 -0300

    Test slim3 fuzzy telegram

---
+++ phinx.php
@@ -0,0 +1,22 @@
+<?php
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+return array(
+    "paths" => array(
+        "migrations" => "migrations"
+    ),
+    "environments" => array(
+        "default_migration_table" => "phinxlog",
+        "default_database" => "default",
+        "default" => array(
+            "adapter" => "mysql",
+            "host" => getenv('RDS_HOSTNAME'),
+            "name" => getenv('RDS_DB_NAME'),
+            "user" => getenv('RDS_USERNAME'),
+            "pass" => getenv('RDS_PASSWORD'),
+            "port" => getenv('RDS_PORT')
+        )
+    )
+);
